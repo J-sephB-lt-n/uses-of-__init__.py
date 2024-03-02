79fa591afc8eea1f4f3b6aadbd4374717a60c98c
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-03-02 21:14:30 +0200

    docs(modules/reptile_creators/): added script names at top of scripts

--- example1/modules/reptile_creators/long_reptiles/__init__.py
+++ example1/modules/reptile_creators/long_reptiles/__init__.py
@@ -1,1 +1,3 @@
+# modules/reptile_creators/long_reptiles/__init__.py
+
 from .create_snake import create_snake
--- example1/modules/reptile_creators/strange_reptiles/__init__.py
+++ example1/modules/reptile_creators/strange_reptiles/__init__.py
@@ -1,1 +1,3 @@
+# modules/reptile_creators/strange_reptiles/__init__.py
+
 from .create_chameleon import create_chameleon
